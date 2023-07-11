03cb0c2711643b5bc25388905ffa9daabaf70ea4
psychehose <psychehose@gmail.com>
2023-07-11 19:00:47 +0900

    Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 
+    private bool isWalking;
+
     // Public 인 경우는 코드 은닉화를 달성할 수 없음
     // 그렇다고 해서 private로 설정하면 에디터에서 옵션을 만질 수가 없음.
     // 이때 [SerializField] 를 사용하면 다른 클래스에서 사용할 수 없고 (코드 은닉) 에디터에서도 사용가능.
@@ -36,8 +38,14 @@ public class Player : MonoBehaviour {
         // 동일한 속도를 보장하기 위해서
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotationSpeed = 10f;
         // 회전을 부드럽게 하는 lerp, slerp
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 }
