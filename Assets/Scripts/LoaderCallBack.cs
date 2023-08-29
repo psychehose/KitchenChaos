cf989c389f70e079fc24150c006c4fd5feba8de8
psychehose <psychehose@gmail.com>
2023-08-29 19:17:22 +0900

    Main Menu, Loading

---
+++ Assets/Scripts/LoaderCallBack.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderCallBack : MonoBehaviour
+{
+    private bool isFirstUpdate = true;
+
+
+    private void Update()
+    {
+        if (isFirstUpdate)
+        {
+
+            isFirstUpdate = false;
+            
+            Loader.LoaderCallBack();
+            
+        }
+    }
+}
